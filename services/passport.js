931f230ee57c13ba5790a8a5b8f799f30a20ddfc
Nitish Ranjan <nitish.ranjan03@gmail.com>
2019-04-29 14:20:50 -0400

    refactoring Google strategy with Async/Await

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // already registered user with the given profile ID
-          done(null, existingUser);
-        } else {
-          // no record of user, make a new record
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
