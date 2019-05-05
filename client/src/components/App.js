623a80681f92dd871c78c907dd9de83ec3cf24fe
Nitish Ranjan <nitish.ranjan03@gmail.com>
2019-05-05 15:02:19 -0400

    main page header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+
+//const Header = () => <h2>Header</h2>;
+const Dashbord = () => {
+  return <h2>Dashbord</h2>;
+};
+
+const SurveyNew = () => <h2>Survey New</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashbord} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="container">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo left">
+              Nitish
+            </a>
+            <ul id="nav-mobile" className="right">
+              <li>
+                <a href="#">Login With Google</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
